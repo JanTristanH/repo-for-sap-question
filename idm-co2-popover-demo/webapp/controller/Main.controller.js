12131bb979834a93b13f55a1c50bee56c4ee3590
Tristan <tristan.heisecke@implico.com>
2022-09-05 08:48:29 +0200

    scaffhold dialog popover

--- idm-co2-popover-demo/webapp/controller/Main.controller.js
+++ idm-co2-popover-demo/webapp/controller/Main.controller.js
@@ -1,15 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/core/Fragment"
+    ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Fragment) {
         "use strict";
 
         return Controller.extend("idmco2popoverdemo.controller.Main", {
             onInit: function () {
 
+            },
+
+            onAfterRendering: function (){
+                setTimeout(()=> this.onOpenDialog(),500)
+            },
+
+            onOpenDialog : function () {
+
+                // create dialog lazily
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "idmco2popoverdemo.view.C02PopoverDialog"
+                    });
+                } 
+                this.pDialog.then(function(oDialog) {
+                    oDialog.open();
+                });
+            },
+            
+            onCloseDialog: function () {
+                this.pDialog.close();
             }
         });
     });
